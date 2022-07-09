4af9d3653eddcd8be6a0d0f904de8b0fe65a7e5b
Oliver Jan Krylow <olver@bugabinga.net>
2022-07-09 16:28:47 +0200

    neovim: use filetype.lua

--- neovim/lua/bugabinga/options/init.lua
+++ neovim/lua/bugabinga/options/init.lua
@@ -2,6 +2,7 @@
 
 require 'bugabinga.options.behaviour'
 require 'bugabinga.options.ui'
+require 'bugabinga.options.file'
 require 'bugabinga.options.keymaps'
 require 'bugabinga.options.autocommands'
 
@@ -15,26 +16,26 @@ vim.g.loaded_perl_provider = 0
 
 -- Disable some in built plugins completely
 local disabled_built_ins = {
-  'netrw',
-  'netrwPlugin',
-  'netrwSettings',
-  'netrwFileHandlers',
-  'gzip',
-  'zip',
-  'zipPlugin',
-  'tar',
-  'tarPlugin',
-  'getscript',
-  'getscriptPlugin',
-  'vimball',
-  'vimballPlugin',
-  '2html_plugin',
-  'logipat',
-  'rrhelper',
-  'spellfile_plugin',
-  'matchit',
-  --'matchparen',
+	'netrw',
+	'netrwPlugin',
+	'netrwSettings',
+	'netrwFileHandlers',
+	'gzip',
+	'zip',
+	'zipPlugin',
+	'tar',
+	'tarPlugin',
+	'getscript',
+	'getscriptPlugin',
+	'vimball',
+	'vimballPlugin',
+	'2html_plugin',
+	'logipat',
+	'rrhelper',
+	'spellfile_plugin',
+	'matchit',
+	--'matchparen',
 }
 for _, plugin in pairs(disabled_built_ins) do
-  vim.g['loaded_' .. plugin] = 1
+	vim.g['loaded_' .. plugin] = 1
 end
