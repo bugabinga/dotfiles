aaa79164acf21a1d872fe99457769daeb5fe7345
Oliver Jan Krylow <olver@bugabinga.net>
2023-04-22 20:52:50 +0200

    helix light theme and xplr

---
+++ xplr/init.lua
@@ -0,0 +1,14 @@
+---@diagnostic disable
+
+-- See https://xplr.dev/en/upgrade-guide
+version = "0.20.1"
+
+local xplr = xplr
+
+-- @diagnostics enable
+
+
+-- Set it to `true` if you want to enable mouse scrolling.
+--
+-- Type: boolean
+xplr.config.general.enable_mouse = false
