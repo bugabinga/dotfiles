4cfd7b7dfa22ca76566d983ed31d1928a6c21085
Oliver Jan Krylow <oliver@bugabinga.net>
2023-07-05 16:07:39 +0200

    nugu: switched colors for debug, error, warn, info in dark and light theme

--- helix/themes/dark-mode/update.sh
+++ helix/themes/dark-mode/update.sh
@@ -5,10 +5,10 @@ set -eu -o pipefail
 
 if [[ "$1" == "default" ]]; then
   # Update Helix Editor config to use light theme.
-  sed -i 's/theme = ".*"/theme = "nugu-light"/' ${HOME}/.config/helix/config.toml
+  sed --in-place --follow-symlinks 's/theme = ".*"/theme = "nugu-light"/' ${HOME}/.config/helix/config.toml
 else
   # Update Helix Editor config to use dark theme.
-  sed -i 's/theme = ".*"/theme = "nugu-dark"/' ${HOME}/.config/helix/config.toml
+  sed --in-place --follow-symlinks 's/theme = ".*"/theme = "nugu-dark"/' ${HOME}/.config/helix/config.toml
 fi
 
 # reload configuration of running helix instances
