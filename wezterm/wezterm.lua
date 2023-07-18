0081b86d85ec059725043e9ad2bcaf43fce58ba9
Oliver Krylow <o.krylow@isp-insoft.de>
2023-07-18 15:58:58 +0200

    wezterm: use the one true font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,16 +14,13 @@ local font_size = 11.0
 local font = wez.font 'IBM Plex Mono'
 
 if hostname == 'x230' then
+  font_size = 11
   enable_wayland = true
-  window_decorations = 'RESIZE'
 elseif hostname == 'pop-os' then
   font_size = 16
 elseif hostname == 'PC-00625' then
-  font_size = 16
-  font = wez.font 'Cascadia Code'
-  window_decorations = 'TITLE|RESIZE'
+  font_size = 12
 end
-
 
 return {
   -- logs key presses
@@ -35,14 +32,11 @@ return {
   warn_about_missing_glyphs = false,
   color_scheme = dark_mode,
   color_schemes = nugu,
-  window_background_opacity = 0.69,
-  -- nightly
-  win32_system_backdrop = 'Acrylic',
   default_cwd = wez.home_dir,
   default_prog = { 'nu' },
   default_cursor_style = 'SteadyBlock',
   cursor_blink_rate = 0,
-  cursor_thickness = '0.6cell',
+  cursor_thickness = '1cell',
   window_decorations = window_decorations,
   enable_tab_bar = true,
   -- if this is hidden, we cannot see the right status area
@@ -52,21 +46,21 @@ return {
   show_update_window = false,
   audible_bell = 'Disabled',
   visual_bell = {
-    fade_in_duration_ms = 75,
-    fade_out_duration_ms = 75,
+    fade_in_duration_ms = 69,
+    fade_out_duration_ms = 69,
     target = 'CursorColor',
   },
   inactive_pane_hsb = {
-    saturation = 0.33,
-    brightness = 0.66,
+    saturation = 0.42,
+    brightness = 0.69,
   },
-  tab_max_width = 24,
+  tab_max_width = 42,
   enable_wayland = enable_wayland,
   window_padding = {
-    left = 12,
-    right = 8,
-    top = 12,
-    bottom = 8,
+    left = 2,
+    right = 2,
+    top = 2,
+    bottom = 0,
   },
   disable_default_key_bindings = true,
   leader = key_binds.leader,
