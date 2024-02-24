0aee04c4dde5b948903126c473162a92d1d53204
Oliver Jan Krylow <oliver@bugabinga.net>
2024-02-24 15:53:27 +0100

    neovim: disable noice

--- neovim/init.lua
+++ neovim/init.lua
@@ -8,33 +8,32 @@
 --        ▀▀▀          █           █   ██         █
 --                    ▀                          ▀
 
-local ok
-local loaded
-
 -- setup debug mode
 local debug = require 'std.debug'
--- debug.toggle()
+debug.toggle()
+
+local ok, loaded
 
 -- set general neovim editor settings
 ok, loaded = pcall( require, 'bugabinga.options' )
-if not ok then vim.print  ( 'error while loading options', loaded )  end
+if not ok then vim.print( 'error while loading options', loaded ) end
 
 -- install plugin manager and declare plugins to use
 ok, loaded = pcall( require, 'bugabinga.lazy' )
-if not ok then vim.print  ( 'error while loading lazy' ,loaded)  end
+if not ok then vim.print( 'error while loading lazy', loaded ) end
 
 -- configures the vim diagnostic subsystem
 ok, loaded = pcall( require, 'bugabinga.diagnostic' )
-if not ok then vim.print  ( 'error while loading diagnostic' ,loaded)  end
+if not ok then vim.print( 'error while loading diagnostic', loaded ) end
 
 -- Defines `TrimTrailingWhitespace` command
 ok, loaded = pcall( require, 'bugabinga.trim' )
-if not ok then vim.print  ( 'error while loading trim' ,loaded)  end
+if not ok then vim.print( 'error while loading trim', loaded ) end
 
 -- visualize marks in signcolumn
 ok, loaded = pcall( require, 'bugabinga.mark' )
-if not ok then vim.print  ( 'error while loading mark' ,loaded)  end
+if not ok then vim.print( 'error while loading mark', loaded ) end
 
 -- setup lsp clients
 ok, loaded = pcall( require, 'bugabinga.lsp' )
-if not ok then vim.print  ( 'error while loading lsp' ,loaded)  end
+if not ok then vim.print( 'error while loading lsp', loaded ) end
