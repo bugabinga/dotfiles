232e8c6d27129cbffa8471422b1b9521c2a4cc19
Oliver Jan Krylow <oliver@bugabinga.net>
2024-04-12 08:51:05 +0200

    neovim: moved neoclip into telescope config

--- neovim/ftplugin/lua.lua
+++ neovim/ftplugin/lua.lua
@@ -2,34 +2,43 @@
 vim.opt_local.include = [[\v<((do|load)file|require)[^''"]*[''"]\zs[^''"]+]]
 vim.opt_local.includeexpr = "substitute(v:fname,'\\.','/','g')"
 for _, path in pairs( vim.api.nvim_list_runtime_paths() ) do
-  vim.opt_local.path:append( path .. '/lua' )
+	vim.opt_local.path:append( path .. '/lua' )
 end
 vim.opt_local.suffixesadd:prepend '.lua'
 
 local map = require 'std.map'
 
 map.normal {
-  description = 'Wrap word under cursor with require call',
-  keys = '<leader>rq',
-  category = 'refactor',
-  -- surround word with '
-  -- insert `require`
-  -- move 3 words
-  command = "ebi'<esc>ea'<esc>bbirequire <esc>www",
-  buffer = true,
-  remap = true,
+	description = 'Wrap word under cursor with require call',
+	keys = '<leader>rq',
+	category = 'refactor',
+	-- surround word with '
+	-- insert `require`
+	-- move 3 words
+	command = "ebi'<esc>ea'<esc>bbirequire <esc>www",
+	buffer = true,
+	remap = true,
 }
 
 --TODO: add debug.print, require, prequire
 prequire 'nvim-surround'.buffer_setup {
-  surrounds = {
-    ['P'] = {
-      add = { 'vim.print(', ')', },
-      find = 'vim%.print%b()',
-      delete = '^(vim%.print%()().-(%))()$',
-      change = {
-        target = '^(vim%.print%()().-(%))()$',
-      },
-    },
-  },
+	surrounds = {
+		['R'] = {
+			add = { "require '", "'", },
+			find = "require%b''",
+			--FIXME: find out how to craft these, those cannot be correct right now
+			delete = "^(require')().-(')()$",
+			change = {
+				target = "^(require')().-('()$",
+			},
+		},
+		['P'] = {
+			add = { 'vim.print(', ')', },
+			find = 'vim%.print%b()',
+			delete = '^(vim%.print%()().-(%))()$',
+			change = {
+				target = '^(vim%.print%()().-(%))()$',
+			},
+		},
+	},
 }
