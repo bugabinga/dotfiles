f61288146b15f9378bd3e08632426474eb0f8dcb
Oliver Jan Krylow <oliver@bugabinga.net>
2024-09-16 19:46:31 +0200

    neovim: tweak plugins startup time nushell: new command for direnv and refreshing env

--- neovim/lua/bugabinga/plugins/comment.lua
+++ neovim/lua/bugabinga/plugins/comment.lua
@@ -1,6 +1,7 @@
-return { {
+return {
   'numToStr/Comment.nvim',
   version = '0.*',
+  lazy = false,
   keys = {
     { 'gc', mode = { 'n', 'v', }, desc = 'Comment toggle linewise', },
     { 'gb', mode = { 'n', 'v', }, desc = 'Comment toggle blockwise', },
@@ -12,4 +13,4 @@ return { {
     local commentstring_avail, commentstring = pcall( require, 'ts_context_commentstring.integrations.comment_nvim' )
     return commentstring_avail and commentstring and { pre_hook = commentstring.create_pre_hook(), } or {}
   end,
-}, }
+}
--- neovim/lua/bugabinga/plugins/last-place.lua
+++ neovim/lua/bugabinga/plugins/last-place.lua
@@ -1,13 +1,11 @@
 local ignored = require 'std.ignored'
 
 return {
-  {
-    'ethanholz/nvim-lastplace',
-    event = { 'VimEnter', 'SessionLoadPost', },
-    opts = {
-      lastplace_ignore_buftype = ignored.buftypes,
-      lastplace_ignore_filetype = ignored.filetypes,
-      lastplace_open_folds = true,
-    },
+  'ethanholz/nvim-lastplace',
+  lazy = false,
+  opts = {
+    lastplace_ignore_buftype = ignored.buftypes,
+    lastplace_ignore_filetype = ignored.filetypes,
+    lastplace_open_folds = true,
   },
 }
