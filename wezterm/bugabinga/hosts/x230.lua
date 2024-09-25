80661fef9b4b77140173d69f857fc6bd0b95349f
Oliver Jan Krylow <oliver@bugabinga.net>
2024-09-25 23:10:53 +0200

    wezterm: window decorations NONE seems to work best for me
    even though the documentation discourages from using that setting
    without the RESIZE flag, fon Fedora 42 GNOME and Hyprland it has been my
    experience, that the sole flag NONE works best.

--- wezterm/bugabinga/hosts/x230.lua
+++ wezterm/bugabinga/hosts/x230.lua
@@ -1,5 +1,6 @@
 local code_font = require 'bugabinga.code_font'
 
-return function(cfg)
-    cfg.font = code_font.random();
+return function ( cfg )
+  cfg.window_decorations = 'NONE'
+  cfg.font = code_font.random();
 end
