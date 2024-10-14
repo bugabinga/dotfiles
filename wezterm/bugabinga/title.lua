d1ae0580777dfd85e323fdb20766fed9ea32a974
Oliver Krylow <o.krylow@isp-insoft.de>
2024-10-14 11:40:32 +0200

    nice font be nice

---
+++ wezterm/bugabinga/title.lua
@@ -0,0 +1,27 @@
+local wez = require 'wezterm'
+
+local icons = {
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  
+}
+wez.on('format-window-title', function(tab, pane, tabs, panes, config)
+  local zoomed = ''
+  if tab.active_pane.is_zoomed then
+    zoomed = '[Z] '
+  end
+
+  local index = ''
+  if #tabs > 1 then
+    index = string.format('[%d/%d] ', tab.tab_index + 1, #tabs)
+  end
+
+  return zoomed .. index .. tab.active_pane.title
+end)
